8bed649303f5d0b18eea44824d59b6de523e9e62
andrea.carbone <andrea.carbone@1c7d64d3-9b28-0410-bae3-039769c3cb81>
2007-02-12 01:37:51 +0000

    WIP

--- trunk/core/pixel_traits.h
+++ trunk/core/pixel_traits.h
@@ -1,6 +1,8 @@
 #ifndef pixel_traits_H_INCLUDED
 #define pixel_traits_H_INCLUDED
-
+//---------------------------------------------------------------------------+
+#include "alcor/core/core.h"
+//---------------------------------------------------------------------------+
 typedef struct iRGB{};
 typedef struct pRGB{};
 typedef struct iBGR{};
@@ -14,49 +16,47 @@ template< >
 class pixel_traits<iRGB>
 {
 public:
-  typedef core::uint8_t     value_type;
+  typedef all::core::uint8_t     value_type;
   typedef value_type*       ptr;
   typedef const ptr*        const_ptr;
 
     //: Is signed
   static bool           is_signed()           {return false;}
-  static unsigned       size_t bytes_per_channel()   {return 1;}
-  static unsigned int   nchannels()            {return 3; }
-
+ 
+  BOOST_STATIC_CONSTANT(std::size_t, bytes_per_channel=1);
+  BOOST_STATIC_CONSTANT(std::size_t, nchannels=3);
   BOOST_STATIC_CONSTANT(std::size_t, size=sizeof(value_type));
-  //BOOST_STATIC_CONSTANT(std::size_t, channel_stride = 1);
 };
 //---------------------------------------------------------------------------+
 template< >
 class pixel_traits<pRGB>
 {
 public:
-  typedef core::uint8_t     value_type;
+  typedef all::core::uint8_t     value_type;
   typedef value_type*       ptr;
   typedef const ptr*        const_ptr;
 
     //: Is signed
   static bool         is_signed()           {return false;}
-  static unsigned size_t bytes_per_channel()   {return 1;}
-  static unsigned int nchannels()            {return 3; }
-
+ 
+  BOOST_STATIC_CONSTANT(std::size_t, bytes_per_channel=1);
+  BOOST_STATIC_CONSTANT(std::size_t, nchannels=3);
   BOOST_STATIC_CONSTANT(std::size_t, size=sizeof(value_type));
-  BOOST_STATIC_CONSTANT(std::size_t, channel_stride = 1);
 };
 //---------------------------------------------------------------------------+
 template< >
 class pixel_traits<GRAY>
 {
 public:
-  typedef uint8_t     value_type;
+  typedef all::uint8_t     value_type;
   typedef value_type* ptr;
   typedef const ptr*  const_ptr;
 
     //: Is signed
   static bool         is_signed()           {return false;}
-  static unsigned size_t bytes_per_channel()   {return 1;}
-  static unsigned int nchannels()            {return 1; }
 
+  BOOST_STATIC_CONSTANT(std::size_t, bytes_per_channel=1);
+  BOOST_STATIC_CONSTANT(std::size_t, nchannels=1);
   BOOST_STATIC_CONSTANT(std::size_t, size=sizeof(value_type));
 };
 //---------------------------------------------------------------------------+
@@ -64,21 +64,71 @@ template< >
 class pixel_traits<DEPTH>
 {
 public:
-  typedef core::single_t      value_type;
+  typedef all::core::single_t      value_type;
   typedef value_type*         ptr;
   typedef const ptr*          const_ptr;
 
     //: Is signed
   static bool         is_signed()           {return true;}
-  static unsigned size_t bytes_per_channel()   {return sizeof(value_type);}
-  static unsigned int nchannels()            {return 3; }
-
+ 
+  BOOST_STATIC_CONSTANT(std::size_t, bytes_per_channel=sizeof(value_type));
+  BOOST_STATIC_CONSTANT(std::size_t, nchannels=3);
   BOOST_STATIC_CONSTANT(std::size_t, size=sizeof(value_type));
 };
 //---------------------------------------------------------------------------+
-//template <typename PIXEL, typename ORDERING>
+template <typename PIXEL>
+class image_of
+{
+public:
+  ///
+  image_of()
+  {
+  }
+
+  ///
+  image_of(size_t height , size_t width)
+  {
+    //
+    height_ = height; 
+    width_ = width;
+    channels_ = PIXEL::nchannels;
+
+    allocate_(height, width);
+  }
+
+  ///
+  image_of(size_t height , size_t width, PIXEL::ptr p)
+  {
+    //
+    height_   = height; 
+    width_    = width;
+    channels_ = PIXEL::nchannels;
+    //
+    allocate_(height_, width_);
+    //
+    memcpy(
+            data_.get(),//Dst
+            p, //Src
+            height_ * width_ * channels_
+            * PIXEL::bytes_per_channel //size
+            );
+  }
+
+private:
+  ///
+  allocate_(size_t height, size_t width)
+  {
+    data_.reset( new PIXEL::value_type[height*width*PIXEL::nchannels );
+  }
+
+  ///
+  boost::shared_array<PIXEL::value_type> data_;
+  ///
+  size_t height_; 
+  size_t width_;
+  size_t channels_;
+};
 
 //---------------------------------------------------------------------------+
-
 
 #endif//pixel_traits_H_INCLUDED
