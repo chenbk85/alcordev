bec7ea27ebe378934d6bfc264821f38829414147
andrea.carbone <andrea.carbone@1c7d64d3-9b28-0410-bae3-039769c3cb81>
2007-02-14 15:59:23 +0000

    Aggiunto progetto matlabipc e classe matlab

---
+++ matlab/@ipcBee/private/close.m
@@ -0,0 +1,5 @@
+function close( self)
+%UNTITLED1 Summary of this function goes here
+%   Detailed explanation goes here
+ipc_bee_mex(self.CLOSE, self.cnx); 
+clear ipc_bee_mex
---
+++ matlab/@ipcBee/private/open.m
@@ -0,0 +1,4 @@
+function open( self)
+%UNTITLED1 Summary of this function goes here
+%   Detailed explanation goes here
+ipc_bee_mex(self.OPEN, self.cnx); 
