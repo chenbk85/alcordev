9441b9c871799fb1f802a1319ecf41c4542bd6f9
andrea.carbone <andrea.carbone@1c7d64d3-9b28-0410-bae3-039769c3cb81>
2007-02-19 10:50:55 +0000

    aggiornamento directed_perception_ptu client server

--- trunk/act/directed_perception_ptu_server_t.cpp
+++ trunk/act/directed_perception_ptu_server_t.cpp
@@ -13,20 +13,48 @@ directed_perception_ptu_server_t::directed_perception_ptu_server_t(std::string& 
 		set_port(port);
 	}
 
-  add_command_handler("resetptu", boost::bind(&directed_perception_ptu_server_t::reset, this, _1, _2));
-	
+  add_command_handler
+    ("resetptu", boost::bind(&directed_perception_ptu_server_t::reset, this, _1, _2));
+  add_command_handler
+    ("pantilt",  boost::bind(&directed_perception_ptu_server_t::pantilt, this, _1, _2));
+  add_command_handler
+    ("getserverdata",  boost::bind(&directed_perception_ptu_server_t::send_pantilt_data, this, _1, _2));
+
 }
 
 void directed_perception_ptu_server_t::assign_ptu(boost::shared_ptr<act::directed_perception_ptu_t> ptu)
 {
-  ptu_sptr = ptu;
+  ptu_sp = ptu;
 }
 
 void directed_perception_ptu_server_t::reset(client_connection_ptr_t, net_packet_ptr_t)
 {
-  if(ptu_sptr.get())
-    ptu_sptr->reset();
+  if(ptu_sp.get())
+    ptu_sp->reset();
 }
+
+void directed_perception_ptu_server_t::pantilt(client_connection_ptr_t, net_packet_ptr_t packet)
+{
+  if(!!ptu_sp)
+  {
+      act::ptu_server_data_t clientdata;
+      clientdata.import(packet);
+      ptu_sp->set_pantilt(clientdata.pan,clientdata.tilt);
+  } 
+}
+
+void directed_perception_ptu_server_t::send_pantilt_data(client_connection_ptr_t cl, net_packet_ptr_t packet)
+{
+  if(!!ptu_sp)
+  {
+    ptu_sp->get_pantilt((float&)ptu_data.pan, (float&)ptu_data.tilt);
+    net_packet_ptr_t ans;
+    ptu_data.pack(ans);
+  	send_answer_packet("getserverdata", cl, ans);
+  }
+
+}
+
 
 
   }}//all::act
--- trunk/act/directed_perception_ptu_server_t.h
+++ trunk/act/directed_perception_ptu_server_t.h
@@ -12,16 +12,26 @@ namespace all {
 class directed_perception_ptu_server_t : public core::server_base_t
 {
 public:
+  ///
   directed_perception_ptu_server_t(std::string& ini_file);
-
+  ///
   void assign_ptu(boost::shared_ptr<act::directed_perception_ptu_t>);
 
 public:
+  ///
+  void pantilt(client_connection_ptr_t, net_packet_ptr_t);
+  ///
 	void reset(client_connection_ptr_t, net_packet_ptr_t);
+  ///
+  void send_pantilt_data(client_connection_ptr_t, net_packet_ptr_t);
+
 
 private:
-  boost::shared_ptr<act::directed_perception_ptu_t> ptu_sptr;
+  ///
+  boost::shared_ptr<act::directed_perception_ptu_t> ptu_sp;
+  ///
   act::ptu_server_data_t ptu_data;
+
 };
 
   }}//all::act
