9e8c559ec9f33e4bc21fc6d2066a2ec193d0ae1d
andrea.carbone <andrea.carbone@1c7d64d3-9b28-0410-bae3-039769c3cb81>
2007-02-24 11:08:45 +0000

    opencv_data_source_t .. per streaming service

---
+++ trunk/sense/opencv_data_source_t.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <alcor/core/jpeg_encoder_t.h>
+#include <alcor/core/stream_source_t.hpp>
+#include <alcor/sense/opencv_grabber_t.h>
+
+namespace all{ namespace sense {
+
+class opencv_data_source_t : public all::core::stream_source_t 
+{
+public:	
+	opencv_data_source_t();
+	
+	void set_quality(unsigned int);
+
+	virtual int get_data(all::core::uint8_ptr*);
+
+private:
+	boost::shared_ptr<all::core::jpeg_encoder_t>     m_encoder;
+  boost::shared_ptr<all::sense::opencv_grabber_t>  m_cam;
+  void init_();
+  core::uint8_sarr image;
+	int m_jpeg_quality;
+};
+
+}}
