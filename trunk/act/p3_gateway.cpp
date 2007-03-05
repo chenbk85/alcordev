bc135cb1766409e0a004c0886aa8eb658995d3cf
andrea.carbone <andrea.carbone@1c7d64d3-9b28-0410-bae3-039769c3cb81>
2007-03-05 03:06:00 +0000

    ok ... me ne vado a letto

--- trunk/act/p3_gateway.cpp
+++ trunk/act/p3_gateway.cpp
@@ -4,36 +4,79 @@
 //---------------------------------------------------------------------------
 namespace all{namespace act{
 //---------------------------------------------------------------------------
-  p3_gateway::p3_gateway()
-  {
-  impl.reset(new detail::p3_gateway_impl());
-  }
+p3_gateway::p3_gateway()
+{
+impl.reset(new detail::p3_gateway_impl());
+}
 //---------------------------------------------------------------------------
-  bool p3_gateway::open(std::string inifile)
+bool p3_gateway::open(std::string inifile)
+{
+  iniWrapper ini;
+  if ( ini.Load(inifile.c_str()) ) 
   {
-    iniWrapper ini;
-    if ( ini.Load(inifile.c_str()) ) 
+    if(ini.GetInt("config:serialmode", 1))
     {
-      if(ini.GetInt("config:serialmode", 1))
-      {
-        char* port = ini.GetStringAsChar("config:comport","COM5");
-        return (impl->serial_connect(port));
-      }
-     
+      char* port = ini.GetStringAsChar("config:comport","COM5");
+      return (impl->serial_connect(port));
     }
-    return false;
   }
-  //---------------------------------------------------------------------------
-  //
-  math::pose2d const& p3_gateway::get_odometry()const
-  {
-    ArPose arpose = impl->m_robot->getPose();
-    math::pose2d mypose;
+  return false;
+}
+//---------------------------------------------------------------------------
+//
+math::pose2d  p3_gateway::get_odometry()
+{
+  ArPose arpose = impl->m_robot->getPose();
+  math::pose2d mypose;
 
-    mypose.set_x1(arpose.getX());
-    mypose.set_x2(arpose.getY());
-    mypose.set_th(arpose.getTh(),math::deg_tag);
-  }
+  mypose.set_x1(arpose.getX());
+  mypose.set_x2(arpose.getY());
+  mypose.set_th(arpose.getTh(),math::deg_tag);
+
+  return mypose;
+}
+//---------------------------------------------------------------------------
+void p3_gateway::set_heading(const math::angle& heading)
+{
+  impl->m_robot->lock();
+  impl->m_robot->setHeading(heading.deg());
+  impl->m_robot->unlock();	
+}
+//---------------------------------------------------------------------------
+void p3_gateway::set_delta_heading(const math::angle& deltaheading)
+{
+  impl->m_robot->lock();
+  impl->m_robot->setDeltaHeading(deltaheading.deg());
+  impl->m_robot->unlock();
+}
+//---------------------------------------------------------------------------
+    ///Enable Stop Action
+void p3_gateway::enable_stop_mode()
+{
+	impl->m_robot->clearDirectMotion();
+	impl->m_stop->activateExclusive();
+	printf("The robot will now just stop.\n");
+}
+//---------------------------------------------------------------------------
+void p3_gateway::enable_follow_mode()
+{
+  impl->m_robot->clearDirectMotion();
+  impl->m_follow->activateExclusive();
+  printf("The robot will now  follow a target.\n");
+}
+//---------------------------------------------------------------------------
+void p3_gateway::set_target_to_follow 
+    (const math::pose2d& target, double speed)
+{
+  //
+  impl->m_ac_follow->setSpeed(speed);
+  //
+  impl->m_ac_follow->setGoalRel(  
+                      target.getP().magnitude()
+                    , target.getP().orientation().deg()
+                    , false, false
+                    );
+}
 //---------------------------------------------------------------------------
   }}//all::act
 //---------------------------------------------------------------------------
